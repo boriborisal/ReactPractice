b66ababb841cc689bca6144ed221a82dfdf656e8
boriborisal <soeunpark0806@gmail.com>
2025-03-15 22:04:30 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+const user = {
+  name: 'Blur',
+  imageUrl: 'https://i.pinimg.com/736x/b5/2a/a8/b52aa815e38bc48f1ea4e378a978426e.jpg',
+  imageSize: 90,
+};
 
-function MyButton() {
+
+
+export default function Profile() {
   return (
-    <button>I'm a button</button>
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user. imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imagesize
+        }}
+      />
+    </>
   );
 }
 
-function AboutPage() {
-  return (
-    <div>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </div>
-  );
-}
 
-export default function MyApp() {
-  return (
-  <div>
-    <h1>Welcome to my app</h1>
-    <MyButton />
-  </div>
-  );
-}
+
+
+// 얘 왜안됨 위에꺼
+// import './App.css';
+// function MyButton() {
+//   return (
+//     <button>I'm a button</button>
+//   );
+// }
+
+
+// export default function MyApp() {
+//   return (
+//   <div>
+//     <h1>Welcome to my app</h1>
+//     <MyButton />
+//   </div>
+//   );
+// }
 
