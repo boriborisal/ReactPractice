87c1249e138c3ee977e41db4b0c0b49323501e0e
boriborisal <soeunpark0806@gmail.com>
2025-03-15 21:48:43 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </div>
+  );
+}
+
+export default function MyApp() {
+  return (
+  <div>
+    <h1>Welcome to my app</h1>
+    <MyButton />
+  </div>
+  );
+}
+
